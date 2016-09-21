55f8544adec5291486e7800263ac0cc0c842a56c
Wenyao Dong <112841520@qq.com>
2016-09-21 16:06:53 +0800

    201609211606

--- install/schema.php
+++ install/schema.php
@@ -12,17 +12,6 @@ global $wpdb, $wp_queries, $charset_collate;
 
 $charset_collate = $wpdb->get_charset_collate();
 
-/**
- * Retrieve the SQL for creating database tables.
- *
- * @since 3.3.0
- *
- * @global wpdb $wpdb WordPress database abstraction object.
- *
- * @param string $scope Optional. The tables for which to retrieve SQL. Can be all, global, ms_global, or blog tables. Defaults to all.
- * @param int $blog_id Optional. The site ID for which to retrieve SQL. Default is the current site ID.
- * @return string The SQL needed to create the requested tables.
- */
 function wp_get_db_schema( $scope = 'all', $blog_id = null ) {
 	global $wpdb;
 
